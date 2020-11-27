4e47394db54d770d3ed4480d8c6f0581d9a8172d
hugo miranda <hugo.miranda.souza@gmail.com>
2020-11-26 21:56:52 -0300

    add footer component

---
+++ src/components/Footer/index.tsx
@@ -0,0 +1,9 @@
+import React from 'react';
+
+import { Footer as FooterComponent } from './styles';
+
+const Footer: React.FC = () => (
+  <FooterComponent>Desenvolvido por Hugo Miranda</FooterComponent>
+);
+
+export default Footer;
---
+++ src/components/Footer/styles.ts
@@ -0,0 +1,12 @@
+import styled from 'styled-components';
+
+export const Footer = styled.footer`
+  width: 100%;
+  height: 64px;
+  margin-top: auto;
+  background: #1b1d29;
+  color: #fff;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+`;
